Fix OBV: use np.sign for direction

v1indicators/volume/obv.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 
 def obv(close: pd.Series, volume: pd.Series) -> pd.Series:
@@ -7,7 +8,7 @@ def obv(close: pd.Series, volume: pd.Series) -> pd.Series:
     if not all(isinstance(x, pd.Series) for x in (close, volume)):
         raise TypeError("close and volume must be pandas Series")
 
-    direction = close.diff().sign()
+    direction = np.sign(close.diff())
 
     return (volume * direction.fillna(0)).cumsum()
 